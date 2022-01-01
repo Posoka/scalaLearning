12e4eaa3958bc857be42ac88df3fcc13b2e0017b
jacekbosiacki <jacek.bosiacki@ocado.com>
2022-01-02 00:14:19 +0100

    final implementation for review

--- src/main/scala/collections/list/List.scala
+++ src/main/scala/collections/list/List.scala
@@ -13,21 +13,20 @@ sealed trait List [+A] {
 
   def head: A
   def last: A
-
-  def headOpt: Option[A] = Some(head)
-  def lastOpt: Option[A] = Some(last)
+  def headOpt: Option[A]
+  def lastOpt: Option[A]
   def tail: List[A]
   def init: List[A]
 
   // should return index of given element
   def indexOf[B >: A](elem: B): Long = {
-    @tailrec def index(list: List[A], e: B, i: Long): Long = {
-      e match {
-        case list.head => i
-        case _ => if (i > this.size) -1 else index(this.tail, e, i+1)
+    head match {
+      case `elem` => 0
+      case _ => tail match {
+        case Nil => -1
+        case _ => tail.indexOf(elem) + 1
       }
     }
-    index(this, elem, 0)
   }
 
   // Returns first n elements of the list
@@ -55,8 +54,16 @@ sealed trait List [+A] {
     init.reverse.cons(last)
   }
 
-  def zip [B] (that: List[B]): List[(A, B)] = {}
-  def zipWithIndex: List[(A, Long)] = {}
+  def zip [B] (that: List[B]): List[(A, B)] = {
+    if (size != that.size) throw new UnsupportedOperationException("zipping lists of different size")
+    tail match {
+      case Nil => List((head, that.head))
+      case _ => tail.zip(that.tail).cons((head, that.head))
+    }
+  }
+  def zipWithIndex: List[(A, Long)] = {
+    zip(List(1L to size: _*))
+  }
 
   def append[B >: A](that: List[B]): List[B]
 
@@ -65,40 +72,46 @@ sealed trait List [+A] {
 
   // High order functions
   // Recursively implement the following methods:
-  def map [B] (f: (A) => B): List[B] = {
-    List(f(head)).append(tail.map(f))
-  }
-
-  def filter (predicate: (A) => Boolean): List[A] = {
+  def map [B] (f: A => B): List[B] = {
     tail match {
-      case Nil => if (predicate(head)) List(head) else Nil
-      case _: List[A] =>
-        predicate(head) match {
-          case true => tail.filter(predicate).cons(head)
-          case false => tail.filter(predicate)
-        }
+      case Nil => List(f(head))
+      case _ => tail.map(f).cons(f(head))
     }
   }
 
-  def count(predicate: (A) => Boolean): Int = {
-    def counter(list: List[A], predicate: (A) => Boolean, acc: Long): Int = {
+  def filter (predicate: A => Boolean): List[A] = {
+    tail match {
+      case Nil => if (predicate(head)) List(head) else Nil
+      case _ =>
+        if (predicate(head)) tail.filter(predicate).cons(head)
+        else tail.filter(predicate)
+    }
+  }
+
+  def count(predicate: A => Boolean): Int = {
+    @tailrec
+    def counter(list: List[A], predicate: A => Boolean, acc: Long): Int = {
       list.tail match {
         case Nil => if (predicate(list.head)) 1 else 0
-        case _: List[A] => counter(list.tail, predicate, acc + predicate(list.head))
+        case _ => counter(list.tail, predicate, if (predicate(list.head)) acc + 1 else acc)
       }
     }
     counter(this, predicate, 0)
   }
-  def find (predicate: (A) => Boolean): Option[A] = {
-    predicate(head) match {
-      case true => Some(head)
-      case false =>
-        tail match {
-          case Nil => None
-          case _: List[A] => tail.find(predicate)}
+  def find(predicate: A => Boolean): Option[A] = {
+    if (predicate(head)) Some(head)
+    else tail match {
+        case Nil => None
+        case _: List[A] => tail.find(predicate)
+      }
+  }
+  def exists(predicate: A => Boolean): Boolean = find(predicate).isDefined
+  def forAll(predicate: A => Boolean): Boolean = {
+    tail match {
+      case Nil => predicate(head)
+      case _ => if (predicate(head)) tail forAll predicate else false
     }
   }
-  def exists(predicate: (A) => Boolean): Boolean = this.find(predicate).isDefined
 
   // Partial function
   // creates a new collection by its partial application to all elements
@@ -108,8 +121,24 @@ sealed trait List [+A] {
   }
 
   // Currying
-  def foldLeft [B] (z: B) (operator: (B, A) => B): B = {}
-  def foldRight [B] (z: B) (operator: (A, B) => B): B = {}
+  def foldLeft [B] (z: B) (operator: (B, A) => B): B = {
+    head match {
+      case Nil => z
+      case _ => tail match {
+        case Nil => operator(z, head)
+        case _ => tail.foldLeft(z)(operator)
+      }
+    }
+  }
+  def foldRight [B] (z: B) (operator: (A, B) => B): B = {
+    last match {
+      case Nil => z
+      case _ => init match {
+        case Nil => operator(last, z)
+        case _ => init.foldRight(z)(operator)
+      }
+    }
+  }
 }
 
 object List {
@@ -127,23 +156,27 @@ object List {
   def empty[A]: List[A] = Nil
 }
 
-case class Cons[+A](override val head: A, next: List[A], override val last: A, prev: List[A]) extends List[A] {
-  override def headOpt: Option[A] = Some(head)
-  override def lastOpt: Option[A] = Some(last)
-  override def tail: List[A] = next
-  override def init: List[A] = prev
+final case class Cons[+A](override val head: A, next: List[A], override val last: A, prev: List[A]) extends List[A] {
+  def headOpt: Option[A] = Some(head)
+  def lastOpt: Option[A] = Some(last)
+  def tail: List[A] = next
+  def init: List[A] = prev
   override def size: Long = next.size + 1
   override def cons[B >: A](a: B): List[B] = {
     Cons(a, tail.cons(head), last, init.cons(a))
   }
-  override def append[B >: A](that: List[B]): List[B] = Cons(head, tail.append(that), that.last, init.append(that.init.cons(last)))
+  override def append[B >: A](that: List[B]): List[B] = {
+    Cons(head, tail.append(that), that.last, init.append(that.init.cons(last)))
+  }
 }
 
 case object Nil extends List[Nothing] {
-  def head: Nothing = _
-  def last: Nothing = _
-  def tail: List[Nothing] = _
-  def init: List[Nothing] = _
+  def head: Nothing = throw new NoSuchElementException("head of empty list")
+  def last: Nothing = throw new NoSuchElementException("last of empty list")
+  def tail: List[Nothing] = throw new UnsupportedOperationException("tail of empty list")
+  def init: List[Nothing] = throw new UnsupportedOperationException("init of empty list")
+  def headOpt: Option[Nothing] = None
+  def lastOpt: Option[Nothing] = None
   def size: Long = 0
   override def cons[B >: Nothing](a: B): List[B] = List(a)
   override def append[B >: Nothing](that: List[B]): List[B] = that
@@ -151,3 +184,4 @@ case object Nil extends List[Nothing] {
 
 
 // something else is also missing ;)?
+// [JB] no idea, what
